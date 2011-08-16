991344874c43fae28d25e4b4ca44d6fbc1adefcd
Sam <sjltaylor@gmail.com>
2011-08-16 09:48:53 +0100

    page and window

--- src/superview.page.js
+++ src/superview.page.js
@@ -2,20 +2,15 @@ Superview.Page = (function () {
   
   var Superview.Page = function () {
     extend(this).mixin(Superview);
-    z.window().resize()
   };
   
   Superview.Page.prototype = {
-    render: function () {
-      this.z().id('page');
-      
-      
-      
-    },
-    render: override(Superview.prototype.render, function (base) {
-      
-    }),
-    
+    fitWindow: function () {
+      this.bindTo(Superview.Window, {
+        width: true,
+        height: true
+      });
+    }
   };
   
   return Superview.Page;
--- src/superview.window.js
+++ src/superview.window.js
@@ -1,24 +1,38 @@
 Superview.Window = (function () {
   
-  var Superview.Window = new Superview();
+  var Superview.Window = new Superview(),
+      $window = z.window(),
+      body = z.body().addClass('superview-window').css({
+        margin: 0,
+        padding: 0,
+        width: '100%',
+        height: '100%'
+      });
+  
+  Superview.Window.z().appendTo(body);
   
   override(Superview.Window, {
-    setParentView :function (base) {
+    setParentView: function (base) {
       throw new Error('cannot set the parent of the window');
     },
     remove: function (base) {
       throw new Error('cannot remove the window');
-    }
+    },
   );
   
   var w = z.window();
   
-  w.resize(function () {
+  function fitToWindow () {
+    z.body.width(w.width());
+    z.body.height(z.height());
     Superview.Window.outerResize({
-      width: z.width(),
-      height: z.height()
-    });    
-  });
+      width: w.width(),
+      height: w.height()
+    });
+  }
+  
+  fitToWindow()
+  w.resize(fitToWindow);
   
   return Superview.Window; 
 })();
